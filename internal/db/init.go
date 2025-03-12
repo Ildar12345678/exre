6b9a6d16c6da16467824b5d80a064e466799ef0d
gen <gen@mail.ru>
2025-03-12 17:13:04 +0500

    db contract is moved to db package

--- internal/db/init.go
+++ internal/db/init.go
@@ -1,18 +1,33 @@
 package db
 
 import (
-	"expenses2/internal/app"
 	"expenses2/internal/db/sqlite"
 	"expenses2/internal/db/mongo"
 	"errors"
+	"time"
+	"expenses2/internal/types"
 )
 
-func NewDB(dbName, path string) (app.DB, error) {
-	switch dbName {
+type DB interface {
+	Initialize() error
+	GetExpenses(time.Time, time.Time) ([]*types.ExpenseShow, error)
+	GetStatistics(time.Time, time.Time) ([]*types.Statistics, error)
+	SearchExpense(string) ([]*types.ExpenseSearch, error)
+	GetCities() ([]string, error)
+	GetCategories() ([]string, error)
+	GetExpensesNames() ([]string, error)
+	AddExpense(*types.ExpenseAdd) error
+	UpdateExpense(*types.ExpenseShow) error
+	DeleteExpense(int) error
+	Close() error
+}
+
+func NewDB(dbType, dbPath, dbName string) (DB, error) {
+	switch dbType {
 	case "sqlite":
-		return sqlite.NewSQLiteDB(path)
+		return sqlite.NewSQLiteDB(dbPath, dbName)
 	case "mongodb":
-		return mongo.NewMongoDB(path)
+		return mongo.NewMongoDB(dbPath, dbName)
 	default:
 		return nil, errors.New("incorrect db choice")
 	}
